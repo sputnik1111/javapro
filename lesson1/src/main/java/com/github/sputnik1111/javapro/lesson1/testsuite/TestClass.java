91d1af60dfd946b979a2950b1f7f6d9a6cadd42f
VTravin <VTravin@inno.tech>
2024-01-29 10:11:31 +0300

    refactor: reformat code

--- lesson1/src/main/java/com/github/sputnik1111/javapro/lesson1/testsuite/TestClass.java
+++ lesson1/src/main/java/com/github/sputnik1111/javapro/lesson1/testsuite/TestClass.java
@@ -10,43 +10,43 @@ import com.github.sputnik1111.javapro.lesson1.annotation.Test;
 public class TestClass {
 
     @BeforeSuite
-    static void beforeAll(){
+    static void beforeAll() {
         System.out.println("Invoke before suite method");
     }
 
     @AfterSuite
-    static void afterAll(){
+    static void afterAll() {
         System.out.println("Invoke after suite method");
     }
 
     @BeforeTest
-    void beforeEachTest(){
+    void beforeEachTest() {
         System.out.println("Invoke before each test method");
     }
 
     @AfterTest
-    void afterEachTest(){
+    void afterEachTest() {
         System.out.println("Invoke after each test method");
     }
 
     @Test(priority = 1)
     @CsvSource("10, Java, 20, true")
-    void test1(Integer number,String str1,Long number2,Boolean flag){
+    void test1(Integer number, String str1, Long number2, Boolean flag) {
         System.out.println("Invoke test1 method");
     }
 
     @Test(priority = 10)
     @CsvSource("10, 20")
-    void test2(Integer number,Double number2){
+    void test2(Integer number, Double number2) {
         System.out.println("Invoke test2 method");
     }
 
     @Test
-    void test3(){
+    void test3() {
         System.out.println("Invoke test3 method");
     }
 
-    void emptyMethod(){
+    void emptyMethod() {
 
     }
 }
